8930027bd4fef1eb2c9f14838c36d2a04ca17622
Darryl Johnson <dandycodes@gmail.com>
2022-09-11 22:43:18 -0400

    main weather cards added css updated 9/11

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
-const searchedButtons = document.querySelector('#search-badges');
+const searchedBadges = document.querySelector('#search-badges');
 const searchButton = document.querySelector('#searchBtn');
 const searchInput = document.querySelector('#city-search');
 const searchedCitiesButton = document.querySelectorAll('.searched-cities-btn');
+
+// below contains elements for main weather card
+const cityCard = document.querySelector('#city-card');
+const dateCard = document.querySelector('#date-card');
+const iconCard = document.querySelector('#icon-card');
+const tempCard = document.querySelector('#temp-card');
+const windCard = document.querySelector('#wind-card');
+const humidityCard = document.querySelector('#humidity-card');
+const uvCard = document.querySelector('#uv-card');
+
