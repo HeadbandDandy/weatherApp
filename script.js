b785be3204d3ae802d6a73e0667a27ccd1283522
Darryl Johnson <dandycodes@gmail.com>
2022-09-11 22:27:46 -0400

    DOM elements added for js file 9/11

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const searchedButtons = document.querySelector('#search-badges');
+const searchButton = document.querySelector('#searchBtn');
+const searchInput = document.querySelector('#city-search');
+const searchedCitiesButton = document.querySelectorAll('.searched-cities-btn');
